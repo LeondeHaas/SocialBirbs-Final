7877a968b0f1c7719e5c318c6d7ab877332d7a0e
Leon de Haas <96114373+LeondeHaas@users.noreply.github.com>
2023-06-07 19:52:01 +0200

    redirecting failed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './Styles/App.css'
 import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/SignUp';
 import AuthDetails from './components/auth/AuthDetails';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+// import Homepage from './components/Homepage';
+
 
 function App() {
   return (
@@ -11,6 +14,7 @@ function App() {
         <h1>Welcome to SocialBirbs!</h1>
         <br></br>
         <SignIn />
+        <button className='google-sign-in'>Sign in using Google</button>
         <br></br>
         <SignUp />
         <br></br>
--- src/components/auth/AuthDetails.jsx
+++ src/components/auth/AuthDetails.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { auth } from '../../firebase'
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 
@@ -6,7 +6,7 @@ const AuthDetails = () => {
     const [authUser, setAuthUser] = useState(null);
 
     useEffect(() => {
-        const listen =  onAuthStateChanged(auth, (user) => {
+        const listen = onAuthStateChanged(auth, (user) => {
             if (user) {
                 setAuthUser(user)
             } else {
@@ -17,21 +17,21 @@ const AuthDetails = () => {
         return () => {
             listen();
         }
-    }, [] );
+    }, []);
 
     const userSignOut = () => {
         signOut(auth).then(() => {
             console.log('signed out successfully')
         }).catch(error => console.log(error))
     }
-  return (
-    <div>
-        { authUser ? <><p>{`You are signed in as ${authUser.email}`}
-          <br></br>
-    <br></br>
-        </p><button className='sign-out' onClick={userSignOut}>Sign Out</button></> : <p>You are currently signed out.</p>}
-    </div>
-  )
+    return (
+        <div>
+            {authUser ? <><p>{`You are signed in as ${authUser.email}`}
+                <br></br>
+                <br></br>
+            </p><button className='sign-out' onClick={userSignOut}>Sign Out</button></> : <p>You are currently signed out.</p>}
+        </div>
+    )
 }
 
 export default AuthDetails
