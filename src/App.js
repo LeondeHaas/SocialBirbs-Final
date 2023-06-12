74b12bb34cb402ae2835dde218a873820ccd5eb3
Wolfy4H <Leon.Kersten@outlook.com>
2023-06-12 10:16:34 +0200

    bye

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import './App.css';
-import SignIn from './components/auth/SignIn';
-import SignUp from './components/auth/SignUp';
-import AuthDetails from './components/auth/AuthDetails';
+import "./App.css";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
+import AuthDetails from "./components/auth/AuthDetails";
 
 function App() {
   return (
     <div className="App">
-     <SignIn />
-     <SignUp />
-     <AuthDetails />
+      <SignIn />
+      <SignUp />
+      <AuthDetails />
     </div>
   );
 }
 
-const [getTaskList, getTaskList] = useState([]);
-cosnt jobCollectionRef = collection(db, "tasks");
+// const [getTaskList, getTaskList] = useState([]);
+// cosnt jobCollectionRef = collection(db, "tasks");
 
-useEffect(() => {
-const getTasks = async () => {
-const data = await getDocs(jobCollectionRef);
-setTaskList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-}
-getTasks();
-},[])
+// useEffect(() => {
+// const getTasks = async () => {
+// const data = await getDocs(jobCollectionRef);
+// setTaskList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+// }
+// getTasks();
+// },[])
 
 export default App;
