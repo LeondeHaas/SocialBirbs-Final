0c95d0ee521c015d57fc610ea94a376043ac79ee
Wolfy4H <Leon.Kersten@outlook.com>
2023-06-12 11:18:28 +0200

    alles in overview.jsx geplaatst

--- src/App.js
+++ src/App.js
@@ -23,45 +23,4 @@ function App() {
   );
 }
 
-// import {getDocs, collection, databaseDoc, doc} from 'firebase/firestore'
-// import { db } from '../firebase-config';
-
-// const [getTitle, setTitle] = useState("");
-// const [getTask, setTask] = useState("");
-// const [getStatus, setStatus] = useState("");
-// const [getTaskList, getTaskList] = useState([]);
-// cosnt jobCollectionRef = collection(db, "tasks");
-
-// useEffect(() => {
-// const getTasks = async () => {
-// const data = await getDocs(jobCollectionRef);
-// setTaskList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-// }
-// getTasks();
-// },[])
-
-// const createJob = async () => {
-//   await addDoc(jocCollecionRef, {
-//     title: getTitle,
-//     task: getTask,
-//     date: getDate,
-//     status: getStatus,
-//     author: { name: auth.currentUser.displayName, id: auth.currentUser.uid },
-//   });
-//   navigate("/");
-// };
-
-// useEffect(()=> {
-//   const getTasks = async () => {
-//     const data = await getDocs(jobCollectionRef);
-//     setTaskList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
-//   }
-//   getTasks();
-// },[])
-
-// const deletePost = async (id) => {
-//   const postDoc = doc(db, "tasks", id);
-//   await deleteDoc(postDoc);
-// };
-
 export default App;
