55feb3d1edeb4edbd9119c52f7093d5725723c9b
Wolfy4H <Leon.Kersten@outlook.com>
2023-06-12 10:14:37 +0200

    hi

--- src/App.js
+++ src/App.js
@@ -13,4 +13,15 @@ function App() {
   );
 }
 
+const [getTaskList, getTaskList] = useState([]);
+cosnt jobCollectionRef = collection(db, "tasks");
+
+useEffect(() => {
+const getTasks = async () => {
+const data = await getDocs(jobCollectionRef);
+setTaskList(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+}
+getTasks();
+},[])
+
 export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth} from "firebase/auth"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+react;
+import { initializaApp } from "firebase/App";
+import { getFirestor } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+//firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBf6ZfY0HsowxLgsWC8BMKM-qH_leSMKiA",
-  authDomain: "socialbirbs-40d97.firebaseapp.com",
-  projectId: "socialbirbs-40d97",
-  storageBucket: "socialbirbs-40d97.appspot.com",
-  messagingSenderId: "119200831280",
-  appId: "1:119200831280:web:567e2b869fe0b7d830daf5"
+  apiKey: "",
+  authDomain: "",
+  datebaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and get a reference to the service
-export const auth = getAuth(app);
+//initializa firebase
+const App = initializaApp(firebaseConfig);
+//initializa firestore (de connectie)
+export const db = getFirestore(app);
