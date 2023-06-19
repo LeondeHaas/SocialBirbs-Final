dc3b18af6a786a4f94653aef459ef9501d85486f
LeondeHaas <leonthedragonslayer123@gmail.com>
2023-06-19 10:57:22 +0200

    test

--- src/components/auth/AuthDetails.jsx
+++ src/components/auth/AuthDetails.jsx
@@ -1,37 +1,52 @@
-import React, { useState, useEffect } from 'react'
-import { auth } from '../../firebase'
+// AuthDetails.jsx
+
+import React, { useState, useEffect } from 'react';
+import { auth } from '../../firebase';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
+import CreatePost from './CreatePost';
 
 const AuthDetails = () => {
-    const [authUser, setAuthUser] = useState(null);
+  const [authUser, setAuthUser] = useState(null);
 
-    useEffect(() => {
-        const listen = onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setAuthUser(user)
-            } else {
-                setAuthUser(null);
-            }
-        });
+  useEffect(() => {
+    const listen = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setAuthUser(user);
+      } else {
+        setAuthUser(null);
+      }
+    });
 
-        return () => {
-            listen();
-        }
-    }, []);
+    return () => {
+      listen();
+    };
+  }, []);
 
-    const userSignOut = () => {
-        signOut(auth).then(() => {
-            console.log('signed out successfully')
-        }).catch(error => console.log(error))
-    }
-    return (
-        <div>
-            {authUser ? <><p>{`You are signed in as ${authUser.email}`}
-                <br></br>
-                <br></br>
-            </p><button className='sign-out' onClick={userSignOut}>Sign Out</button></> : <p>You are currently signed out.</p>}
-        </div>
-    )
-}
+  const userSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log('signed out successfully');
+      })
+      .catch((error) => console.log(error));
+  };
 
-export default AuthDetails
+  return (
+    <div>
+      {authUser ? (
+        <>
+          <p>{`You are signed in as ${authUser.email}`}</p>
+          <br />
+          <CreatePost />
+          <br />
+          <button className="sign-out" onClick={userSignOut}>
+            Sign Out
+          </button>
+        </>
+      ) : (
+        <p>You are currently signed out.</p>
+      )}
+    </div>
+  );
+};
+
+export default AuthDetails;
